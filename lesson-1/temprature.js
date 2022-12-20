3d799f18392d2ce635aff0cc7358ef4b1a346ec5
Huzaifa yusuf <huothyfayusuf1993@gmail.com>
2022-12-20 16:56:03 +0000

    add function

--- lesson-1/temprature.js
+++ lesson-1/temprature.js
@@ -1,7 +1,10 @@
-function convertFromFahrenheitToCelsius(){  
+function fahrenheitToCelsius(fahrenheit){  
+  
 }
 
-function convertFromCelsiusToFahrenheit(){  
+function celsiusToFahrenheit(celsius){  
+    var fahrenheit = celsius * 9/5 + 32;
+    return fahrenheit;
 }
 
 
